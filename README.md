f0dc7e52def84f3a876e9abacdb0d208292dcc03
chehin <chehinjuan@gmail.com>
2022-01-13 20:13:35 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# infirmary-soft
+
+https://drive.google.com/drive/folders/1IxZAnn6EHXnGdQdjareAq2zPlucjObQN?usp=sharing
---
+++ front/src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnDestroy } from '@angular/core';
+import { Router, ActivationEnd, ActivatedRoute } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-breadcrumbs',
+  templateUrl: './breadcrumbs.component.html',
+  styles: [
+  ]
+})
+export class BreadcrumbsComponent {
+
+  public titulo: string;
+  // public tituloSubs$: Subscription;
+
+
+  constructor( private router: Router, private route: ActivatedRoute ) {
+
+    // this.tituloSubs$ = 'pro';
+
+
+    /*this.getArgumentosRuta()
+                        .subscribe( ({ titulo: any }) => {
+                            this.titulo = titulo;
+                            document.title = `AdminPro - ${ titulo }`;
+                        });
+  }*/
+
+/*
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
+  }*/
+
+
+  /*getArgumentosRuta(): any {
+    return;
+
+    return this.router.events
+      .pipe(
+        filter( event => event instanceof ActivationEnd ),
+        filter( (event: ActivationEnd) => event.snapshot.firstChild === null  ),
+        map( (event: ActivationEnd) => event.snapshot.data ),
+      );
+  }*/
+}
+
+
+}
